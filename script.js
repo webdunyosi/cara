4276b769017da433b6ee6ae12c042e5785f57e6e
webdunyosi <alimardontoshpulatov1978@gmail.com>
2025-11-28 21:22:54 +0500

    Refactor code structure for improved readability and maintainability

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 // Script for navigation bar
+let navbarList = document.querySelector(".navbar-list")
+let menuButton = document.querySelector(".menu")
+menuButton.addEventListener("click", () => {
+  navbarList.classList.toggle("active")
+})
